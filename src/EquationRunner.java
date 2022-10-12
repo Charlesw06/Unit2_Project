18c4307acd9590316a061989bcad08bef5066bf8
BT_4N2_20 <cwalford3884@bths.edu>
2022-10-12 12:29:47 -0400

    Updated and added details to design document

--- src/EquationRunner.java
+++ src/EquationRunner.java
@@ -1,7 +1,4 @@
 import java.util.Scanner;
-
-import static java.lang.Integer.parseInt;
-
 public class EquationRunner {
     public static void main(String[] args) {
         System.out.println("Welcome to the Linear Equation calculator.");
@@ -10,15 +7,15 @@ public class EquationRunner {
         System.out.print("Enter the first x-y coordinate point: ");
         String userPoint1 = point1Input.nextLine();
 
-        int x1 = parseInt(userPoint1.substring(1, userPoint1.indexOf(",")));
-        int y1 = parseInt(userPoint1.substring(userPoint1.indexOf(",") + 1, userPoint1.length() - 1));
+        int x1 = Integer.parseInt(userPoint1.substring(1, userPoint1.indexOf(",")));
+        int y1 = Integer.parseInt(userPoint1.substring(userPoint1.indexOf(",") + 1, userPoint1.length() - 1));
 
         Scanner point2Input = new Scanner(System.in);
         System.out.print("Enter the first x-y coordinate point: ");
         String userPoint2 = point2Input.nextLine();
 
-        int x2 = parseInt(userPoint2.substring(1, userPoint2.indexOf(",")));
-        int y2 = parseInt(userPoint2.substring(userPoint2.indexOf(",") + 1, userPoint2.length() - 1));
+        int x2 = Integer.parseInt(userPoint2.substring(1, userPoint2.indexOf(",")));
+        int y2 = Integer.parseInt(userPoint2.substring(userPoint2.indexOf(",") + 1, userPoint2.length() - 1));
 
         LinearEquation userEquation = new LinearEquation(x1, x2, y1, y2);
         System.out.println(userEquation.toString());
