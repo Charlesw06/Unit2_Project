e66ee0dad880f434b71b229db6f16854a3f7ac35
BT_4N2_20 <cwalford3884@bths.edu>
2022-10-12 12:15:08 -0400

    Completed EquationRunner class to interact with user

--- src/EquationRunner.java
+++ src/EquationRunner.java
@@ -1,6 +1,32 @@
 import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
+
 public class EquationRunner {
     public static void main(String[] args) {
+        System.out.println("Welcome to the Linear Equation calculator.");
 
+        Scanner point1Input = new Scanner(System.in);
+        System.out.print("Enter the first x-y coordinate point: ");
+        String userPoint1 = point1Input.nextLine();
+
+        int x1 = parseInt(userPoint1.substring(1, userPoint1.indexOf(",")));
+        int y1 = parseInt(userPoint1.substring(userPoint1.indexOf(",") + 1, userPoint1.length() - 1));
+
+        Scanner point2Input = new Scanner(System.in);
+        System.out.print("Enter the first x-y coordinate point: ");
+        String userPoint2 = point2Input.nextLine();
+
+        int x2 = parseInt(userPoint2.substring(1, userPoint2.indexOf(",")));
+        int y2 = parseInt(userPoint2.substring(userPoint2.indexOf(",") + 1, userPoint2.length() - 1));
+
+        LinearEquation userEquation = new LinearEquation(x1, x2, y1, y2);
+        System.out.println(userEquation.toString());
+
+        Scanner newX = new Scanner(System.in);
+        System.out.print("Enter an x-value: ");
+        double userXCord = newX.nextDouble();
+
+        System.out.println("Solved coordinate point: " + userEquation.newPoint(userXCord));
     }
 }
--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,4 +1,6 @@
+import java.text.DecimalFormat;
 public class LinearEquation {
+    DecimalFormat twoDForm = new DecimalFormat("#.00");
     private int x1;
     private int x2;
     private int y1;
@@ -12,25 +14,33 @@ public class LinearEquation {
     }
 
     public double distance() {
-        return Math.sqrt(Math.pow(x2 - x1, 2) - Math.pow(y2 -y1, 2));
+        return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
     }
 
     public double slope() {
-        return (y2 - y1) / (x2 - x1);
+        return Math.round((y2 - y1) / (x2 - x1));
     }
 
     public double yIntercept() {
-        return y1 - (slope() * x1);
+        return Math.round(y1 - (slope() * x1));
     }
 
     public String equationOfLine() {
-        return "y = " + slope() + "x + " + yIntercept();
+        return "y = " + (y2 - y1) + "/" + (x2 - x1) + "x + " + twoDForm.format(yIntercept());
     }
 
     public String toString() {
         String firstPoint = "First point: (" + x1 + "," + y1 + ")";
         String secondPoint = "Second point: (" + x2 + "," + y2 + ")";
-        String slope = "Slope of line: " + slope();
-        return firstPoint + "\n" + secondPoint + "\n";
+        String slope = "Slope of line: " + twoDForm.format(slope());
+        String yIntercept = "Y-Intercept: " + twoDForm.format(yIntercept());
+        String lineEquation = "Equation of line: " + equationOfLine();
+        String distance = "Distance between points: " + twoDForm.format(distance());
+        return firstPoint + "\n" + secondPoint + "\n" + slope + "\n" + yIntercept + "\n" + lineEquation + "\n" + distance;
+    }
+
+    public String newPoint(double userX) {
+        double userY = slope() * userX + yIntercept();
+        return "(" + userX + "," + userY + ")";
     }
 }
