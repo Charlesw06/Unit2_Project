d25f005100a802d1c55e7627b0014a3019b7f9cd
Charlesw06 <cwalford3884@bths.edu>
2022-10-18 15:04:58 -0400

    Fixed two decimal format in newPoint() and edited comments

--- src/EquationRunner.java
+++ src/EquationRunner.java
@@ -1,38 +1,39 @@
-import java.util.Scanner;
 // Imports scanner utility
+import java.util.Scanner;
+
 public class EquationRunner {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Linear Equation calculator.");
         // Welcomes user
+        System.out.println("Welcome to the Linear Equation calculator.");
 
+        // Prompts user using scanner, initializes new String variable to user's input
         Scanner point1Input = new Scanner(System.in);
         System.out.print("Enter the first x-y coordinate point: ");
         String userPoint1 = point1Input.nextLine();
-        // Prompts user using scanner, initializes new String variable to user's input
 
+        // Initializes x1 and y1 to corresponding values in user's answer
         int x1 = Integer.parseInt(userPoint1.substring(1, userPoint1.indexOf(",")));
         int y1 = Integer.parseInt(userPoint1.substring(userPoint1.indexOf(",") + 1, userPoint1.length() - 1));
-        // Initializes x1 and y1 to corresponding values in user's answer
 
+        // Prompts user using scanner, initializes new String variable to user's input
         Scanner point2Input = new Scanner(System.in);
         System.out.print("Enter the first x-y coordinate point: ");
         String userPoint2 = point2Input.nextLine();
-        // Prompts user using scanner, initializes new String variable to user's input
 
+        // Initializes x2 and y2 to corresponding values in user's answer
         int x2 = Integer.parseInt(userPoint2.substring(1, userPoint2.indexOf(",")));
         int y2 = Integer.parseInt(userPoint2.substring(userPoint2.indexOf(",") + 1, userPoint2.length() - 1));
-        // Initializes x2 and y2 to corresponding values in user's answer
 
+        // Creates new LinearEquation object and prints out the object's information
         LinearEquation userEquation = new LinearEquation(x1, x2, y1, y2);
         System.out.println(userEquation.toString());
-        // Creates new LinearEquation object and prints out the object's information
 
+        // Prompts user for x-value using scanner, initializes new double variable to user's input
         Scanner newX = new Scanner(System.in);
         System.out.print("Enter an x-value: ");
         double userXCord = newX.nextDouble();
-        // Prompts user for x-value using scanner, initializes new double variable to user's input
 
-        System.out.println("Solved coordinate point: " + userEquation.newPoint(userXCord));
         // Prints corresponding coordinates to user's x-value
+        System.out.println("Solved coordinate point: " + userEquation.newPoint(userXCord));
     }
 }
--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,43 +1,45 @@
-import java.text.DecimalFormat;
 // Imports Decimal format
-public class LinearEquation {
-    DecimalFormat twoDForm = new DecimalFormat("#.00");
-    // Sets decimal format to two decimal places
+import java.text.DecimalFormat;
 
+public class LinearEquation {
+    // Sets decimal format to two decimal places
+    DecimalFormat twoDForm = new DecimalFormat("#.00");
+
+    // Creates instance variables x1, x2, y1, y2 to represent x-y coordinate values
     private int x1;
     private int x2;
     private int y1;
     private int y2;
-    // Creates instance variables x1, x2, y1, y2 to represent x-y coordinate values
 
+    // Constructor that initializes instance variables to corresponding values
     public LinearEquation(int x1, int x2, int y1, int y2) {
         this.x1 = x1;
         this.x2 = x2;
         this.y1 = y1;
         this.y2 = y2;
     }
-    // Constructor that initializes instance variables to corresponding values
 
+    // Calculates and returns distance between two points
     public double distance() {
         return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
     }
-    // Calculates and returns distance between two points
 
+    // Calculates and returns slope of line created by two points
     public double slope() {
         return Double.parseDouble(twoDForm.format(((double) (y2 - y1)) / (x2 - x1)));
     }
-    // Calculates and returns slope of line created by two points
 
+    // Calculates and returns y-intercept of line created by two points
     public double yIntercept() {
         return Double.parseDouble(twoDForm.format(y1 - (slope() * x1)));
     }
-    // Calculates and returns y-intercept of line created by two points
 
+    // Returns equation of the line created by two points
     public String equationOfLine() {
         return "y = " + (y2 - y1) + "/" + (x2 - x1) + "x + " + twoDForm.format(yIntercept());
     }
-    // Returns equation of the line created by two points
 
+    // Returns information about the line created between two points
     public String toString() {
         String firstPoint = "First point: (" + x1 + "," + y1 + ")";
         String secondPoint = "Second point: (" + x2 + "," + y2 + ")";
@@ -47,11 +49,10 @@ public class LinearEquation {
         String distance = "Distance between points: " + twoDForm.format(distance());
         return firstPoint + "\n" + secondPoint + "\n" + slope + "\n" + yIntercept + "\n" + lineEquation + "\n" + distance;
     }
-    // Returns information about the line created between two points
 
+    // Calculates and returns coordinates corresponding to x-value inputted
     public String newPoint(double userX) {
         double userY = slope() * userX + yIntercept();
-        return "(" + userX + "," + userY + ")";
+        return "(" + twoDForm.format(userX) + "," + twoDForm.format(userY) + ")";
     }
-    // Calculates and returns coordinates corresponding to x-value inputted
 }
